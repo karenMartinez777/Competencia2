3bb9dbcd8639bc3b46cef456b8f810085ceaadc6
Karen Tatiana Martinez Ordoñez <ktmo1701@gmail.com>
2024-04-04 12:43:56 -0500

    2 menu de mi aplicacion

---
+++ src/main/java/Atleta.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 1059236799
+ */
+public class Atleta {
+    private String Nombre;
+    private String Nacionalidad;
+    private int hora;
+    private int minutos;
+
+    public Atleta() {
+    }
+
+    public Atleta(String Nombre, String Nacionalidad, int hora, int minutos) {
+        this.Nombre = Nombre;
+        this.Nacionalidad = Nacionalidad;
+        this.hora = hora;
+        this.minutos = minutos;
+    }
+    
+    
+    
+}
